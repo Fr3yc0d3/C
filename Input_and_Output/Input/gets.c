488b0a8fa67524ac17fc3ef727d74e6ed4d0e222
fr3y <free3y@gmail.com>
2021-09-01 23:17:44 +0530

    Sum

---
+++ Input_and_Output/Input/gets.c
@@ -0,0 +1,13 @@
+/*
+ * taking users input by using gets() function
+ * we will use array here xD
+*/
+
+#include <stdio.h>
+int main(){
+    char a[100];
+    gets(a);
+    printf("you have entered --> %s", a);
+    printf("Freyc0d3 is just for personal stuffs");
+    return 0;
+}
---
+++ Input_and_Output/Input/scanf.c
@@ -0,0 +1,12 @@
+/*
+ * Now lets cal sum of 2 numbers with thehelp of scanf
+*/
+
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("Enter two numbers sir:");
+    scanf("%d %d", &a, &b);
+    printf("\nSum is %d", a+b);
+    return 0;
+}
